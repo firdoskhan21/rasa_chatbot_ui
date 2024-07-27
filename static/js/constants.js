9c49b2e25c03e282acc417b8682b81ad290a46a9
firdos khan <wisefirdos786@gmail.com>
2024-07-27 19:22:13 +0200

    added new endpont

--- static/js/constants.js
+++ static/js/constants.js
@@ -24,6 +24,6 @@ const taskPorts =
     ? 5009
     : 5010;
 // const rasa_server_url = "http://34.224.98.239/task1/webhooks/rest/webhook";
-const rasa_server_url = `http://192.168.1.101:${taskPorts}/webhooks/rest/webhook`;
+const rasa_server_url = `https://7333-134-109-92-76.ngrok-free.app/webhooks/rest/webhook`;
 
 const sender_id = uuidv4();
