55f3b61f894c710e843edab3dcb6bf27c9ace94c
Firdos Khan <47527216+firdoskhan21@users.noreply.github.com>
2025-01-19 16:21:12 +0100

    Update constants.js

--- static/js/constants.js
+++ static/js/constants.js
@@ -11,6 +11,7 @@ function getQueryParams() {
 
 const { id: userId, type: task_name } = getQueryParams();
 
+/* 
 const uriPorts = {
   task1_darkpattern: 5006,
   task1_regular: 5005,
@@ -18,18 +19,20 @@ const uriPorts = {
   task2_regular: 5006,
   task3_darkpattern: 5005,
   task3_regular: 5007,
-};
+}; 
 
-// const uriMappings = {
-//   task1_darkpattern: "https://3795-134-109-92-76.ngrok-free.app", //5006
-//   task1_regular: "https://74f2-134-109-92-76.ngrok-free.app", //5005
-//   task2_darkpattern: "https://e573-134-109-92-76.ngrok-free.app", //5007
-//   task2_regular: "https://3795-134-109-92-76.ngrok-free.app", // 5006
-//   task3_darkpattern: "https://74f2-134-109-92-76.ngrok-free.app", //5005
-//   task3_regular: "https://e573-134-109-92-76.ngrok-free.app", //5007
-// };
+*/
 
-const rasa_server_url = `http://localhost:${uriPorts[task_name]}/webhooks/rest/webhook`;
-// const rasa_server_url = `${uriMappings[task_name]}/webhooks/rest/webhook`;
+ const uriMappings = {
+   task1_darkpattern: "https://79a6-134-109-92-76.ngrok-free.app", //5006
+   task1_regular: "https://2a6e-134-109-92-76.ngrok-free.app", //5005
+   task2_darkpattern: "https://7468-134-109-92-76.ngrok-free.app", //5007
+   task2_regular: "https://79a6-134-109-92-76.ngrok-free.app", // 5006
+   task3_darkpattern: "https://2a6e-134-109-92-76.ngrok-free.app", //5005
+   task3_regular: "https://7468-134-109-92-76.ngrok-free.app", //5007
+ };
+
+// const rasa_server_url = `http://localhost:${uriPorts[task_name]}/webhooks/rest/webhook`;
+const rasa_server_url = `${uriMappings[task_name]}/webhooks/rest/webhook`;
 
 const sender_id = uuidv4();
